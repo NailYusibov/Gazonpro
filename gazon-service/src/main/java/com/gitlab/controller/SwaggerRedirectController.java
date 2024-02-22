ddd03cce7ec11f370aab12721a89955f702d9fad
rasulovtim <timmprogramming@gmail.com>
2024-02-22 20:26:55 +0500

    #158 добавил контроллер для редиректа с "/" на эндпоинт со сваггером

---
+++ gazon-service/src/main/java/com/gitlab/controller/SwaggerRedirectController.java
@@ -0,0 +1,13 @@
+package com.gitlab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SwaggerRedirectController {
+    @GetMapping("/")
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui/");
+    }
+}
