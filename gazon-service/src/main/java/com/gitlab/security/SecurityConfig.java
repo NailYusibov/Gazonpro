2abb28a6e38b42cf5da3975d3666141bd7c355ed
Dani <120296813+dane118@users.noreply.github.com>
2024-03-04 01:25:50 +0700

    primary-keycloak-realization-filter

---
+++ gazon-service/src/main/java/com/gitlab/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+//package com.gitlab.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .authorizeHttpRequests()
+//                .anyRequest()
+//                .authenticated();
+//
+//        http.oauth2ResourceServer().jwt();
+//        http
+//                .sessionManagement()
+//                .sessionCreationPolicy(STATELESS);
+//        return http.build();
+//    }
+//}
+//
+//
+////import org.springframework.context.annotation.Bean;
+////import org.springframework.context.annotation.Configuration;
+////import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+////import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+////import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+////import org.springframework.security.config.http.SessionCreationPolicy;
+////import org.springframework.security.web.SecurityFilterChain;
+////
+////@Configuration
+////@EnableWebSecurity
+////public class SecurityConfig extends WebSecurityConfigurerAdapter {
+////
+////    @Bean
+////    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+////        //аутентификация всех запросов
+////        http
+////                .csrf().disable()
+////                .authorizeHttpRequests()
+////                .anyRequest()
+////                .authenticated();
+////        // используется сервер ресурсов OAuth2 и наличие JWT токена
+////        http
+////                .oauth2ResourceServer()
+////                .jwt();
+////        http
+////                .sessionManagement()
+////                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+////        return http.build();
+////
+////    }
+////}
