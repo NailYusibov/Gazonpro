9c3d7f807b99b24b3545b394b70b4c51164775c3
Vladislav <vladikul98@gmail.com>
2024-05-07 17:43:25 +0300

    #136 - Checking functionality ReviewImage Rest

--- common/src/main/java/com/gitlab/dto/ReviewImageUpdateDto.java
+++ common/src/main/java/com/gitlab/dto/ReviewImageUpdateDto.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.ReadOnlyProperty;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -14,7 +13,6 @@ public class ReviewImageUpdateDto {
     @ReadOnlyProperty
     private Long id;
 
-    @NotNull(message = "ReviewImage's reviewId should not be empty")
     private Long reviewId;
 
     @Size(max = 256, message = "Length of ReviewImage's name should be between 1 and 256 characters")
