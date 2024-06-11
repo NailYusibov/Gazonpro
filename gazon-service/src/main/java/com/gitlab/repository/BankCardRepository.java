db9572c3634cbded96b02272831f4a158bb52537
Aleksei964 <igo-anikeev@yandex.ru>
2024-06-11 23:14:07 +0400

    #197 Add sorting by id when receiving a BankCard from the database

--- gazon-service/src/main/java/com/gitlab/repository/BankCardRepository.java
+++ gazon-service/src/main/java/com/gitlab/repository/BankCardRepository.java
@@ -1,9 +1,25 @@
 package com.gitlab.repository;
 
 import com.gitlab.model.BankCard;
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BankCardRepository extends JpaRepository<BankCard, Long> {
+
+    @Override
+    @NonNull
+    @Query("SELECT u FROM BankCard u order by u.id asc")
+    List<BankCard> findAll();
+
+    @Override
+    @NonNull
+    @Query("SELECT u FROM BankCard u order by u.id asc")
+    Page<BankCard> findAll(Pageable pageable);
 }
