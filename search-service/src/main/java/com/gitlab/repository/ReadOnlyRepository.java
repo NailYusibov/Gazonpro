3ec12ee34bfc613411b33ddb32c0f0e00061beb6
danila <danila_sm@mail.ru>
2024-07-20 22:22:59 +0300

    added ReadOnlyRepository

---
+++ search-service/src/main/java/com/gitlab/repository/ReadOnlyRepository.java
@@ -0,0 +1,17 @@
+package com.gitlab.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@NoRepositoryBean
+public interface ReadOnlyRepository<T, ID> extends Repository<T, ID> {
+    Optional<T> findById(ID id);
+    List<T> findAll();
+    Page<T> findAll(Pageable pageable);
+}
