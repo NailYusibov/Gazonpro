a96f8efb54584008e66e47f0c72cf05e0ae9de9f
Denis Pronin <de.pronin@gmail.com>
2024-07-24 11:36:00 +0500

    #183 updated BankCardRestController security

--- gazon-microservice-starter/src/main/java/com/gitlab/exception/handler/ForbiddenException.java
+++ gazon-microservice-starter/src/main/java/com/gitlab/exception/handler/ForbiddenException.java
@@ -4,8 +4,8 @@ package com.gitlab.exception.handler;
 import org.springframework.http.HttpStatus;
 
 public class ForbiddenException extends BusinessException {
+
     public ForbiddenException() {
         super(HttpStatus.FORBIDDEN, "Forbidden: Access is denied.");
     }
-
 }
