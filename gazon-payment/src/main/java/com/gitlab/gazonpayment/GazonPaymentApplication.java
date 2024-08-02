3d5aad4295d128bb20119c1d913f4f35ef366cce
Daniil Kolomychenko <shanebetterfather@gmail.com>
2024-08-02 17:57:15 +0300

    #162 - Start tracking other files as well

---
+++ gazon-payment/src/main/java/com/gitlab/gazonpayment/GazonPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.gitlab.gazonpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GazonPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GazonPaymentApplication.class, args);
+	}
+
+}
---
+++ gazon-payment/src/test/java/com/gitlab/gazonpayment/GazonPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.gazonpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GazonPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
