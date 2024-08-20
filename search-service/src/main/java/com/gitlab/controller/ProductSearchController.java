d4fbeb0f7d070790ea67a2f54f709a0d27b290e5
danila <danila_sm@mail.ru>
2024-08-20 18:36:49 +0300

    added pagination

--- search-service/src/main/java/com/gitlab/controller/ProductSearchController.java
+++ search-service/src/main/java/com/gitlab/controller/ProductSearchController.java
@@ -4,6 +4,8 @@ import com.gitlab.controllers.api.rest.ProductSearchRestApi;
 import com.gitlab.dto.ProductDto;
 import com.gitlab.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +19,14 @@ public class ProductSearchController implements ProductSearchRestApi {
     private final ProductService productService;
 
     @Override
-    public ResponseEntity<List<ProductDto>> search(String name) throws InterruptedException {
+    public ResponseEntity<List<ProductDto>> searchOptionalPaginate(String name, Integer page, Integer size) throws InterruptedException {
+        if (page != null && size != null) {
+            Pageable pageable = PageRequest.of(page, size);
+            List<ProductDto> products = productService.findByNameIgnoreCaseContaining(name, pageable).getContent();
+            return products.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(products);
+        }
         List<ProductDto> products = productService.findByNameIgnoreCaseContaining(name);
-
-
-        return !products.isEmpty() ?
-                ResponseEntity.ok(products) :
-        ResponseEntity.noContent().build();
+        return products.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(products);
     }
+
 }
